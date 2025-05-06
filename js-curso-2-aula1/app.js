f11325e7a44238c84eb09bfd3cab26874c8cbc1c
Diego Daniel <diegodanielpaiva@gmail.com>
2025-05-06 15:20:44 -0400

    Interagindo com html

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,6 @@
+//estudando funções e manipulação
+let titulo = document.querySelector('h1');
+titulo.innerHTML =  'Jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10'
