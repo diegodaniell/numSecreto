964d315b34f5229ac33649226aa67cc22c132438
Diego Daniel <diegodanielpaiva@gmail.com>
2025-05-06 15:47:43 -0400

    Melhorando codigo

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,6 +1,18 @@
 //estudando funções e manipulação
-let titulo = document.querySelector('h1');
-titulo.innerHTML =  'Jogo do numero secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML =  'Jogo do numero secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero entre 1 e 10'
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+function exibirTextoNaTela(tag, texto) {   
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1','Jogo do numero secreto');
+exibirTextoNaTela('p','Escolha um numero entre 1 e 10');
+
+function verificarChute() {
+    console.log('O botão foi clicado!');
+}
