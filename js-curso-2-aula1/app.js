ea51e63c838396acc09c8d57340366a90906ebeb
Diego Daniel <diegodanielpaiva@gmail.com>
2025-05-06 15:49:22 -0400

    apagando comentarios

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,10 +1,3 @@
-//estudando funções e manipulação
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML =  'Jogo do numero secreto';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
-
 function exibirTextoNaTela(tag, texto) {   
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
